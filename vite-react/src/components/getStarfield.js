799e773709ad4b8fe12b8caf341bc62461c00515
Sanju Rohilla <155614568+SanjuRohilla@users.noreply.github.com>
2026-07-25 17:17:36 +0530

    Update texture path for starfield material

--- vite-react/src/components/getStarfield.js
+++ vite-react/src/components/getStarfield.js
@@ -36,7 +36,7 @@ export default function getStarfield({ numStars = 8000 } = {}) {
     size: 1.6,
     vertexColors: true,
     map: new THREE.TextureLoader().load(
-      "./public/textures/circle.png"
+      "/textures/circle.png"
       // "vite-react/public/assets/Sanju.jpg"
     ),
   });
